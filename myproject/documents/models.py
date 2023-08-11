0dd98171364c574542be6040f612f3a64ee853b8
Hatem <hatemgad98@gmail.com>
2023-08-11 03:35:02 +0300

    - Added Image model and serializer - Added PDF model and serializer

--- myproject/documents/models.py
+++ myproject/documents/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+class Image(models.Model):
+    file = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    
+
+class PDF(models.Model):
+    file = models.FileField(upload_to='pdfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    # class Meta:
+    #     ordering = ['file']
+     
+    # def __str__(self):
+    #     return self.file
--- myproject/documents/serializers.py
+++ myproject/documents/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
 from .models import Image, PDF
 
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('file',)
+
+class PDFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PDF
+        fields = ('file',)
