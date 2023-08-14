d4e1830afb9db2c48fa90f36cd9a65d5369baa15
Hatem <hatemgad98@gmail.com>
2023-08-14 22:35:49 +0300

    Add exception handling

--- myproject/documents/views.py
+++ myproject/documents/views.py
@@ -5,14 +5,12 @@ from rest_framework.decorators import api_view
 from rest_framework.parsers import MultiPartParser
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-from PIL import Image as PILImage
 import io
 import os
-from PIL import Image as PILImage
-from PyPDF2 import PdfReader
 import PyPDF2
 import base64
 import fitz
+from PIL import Image
 from pdf2image import convert_from_path
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
@@ -28,8 +26,8 @@ from .serializers import (
 from .utils import get_num_pdf_pages, is_pdf, is_image
 
 
-# Views here
 
+# Views here
 
 @api_view(["POST"])
 def upload_file(request):
@@ -159,7 +157,12 @@ def get_all_pdfs(request):
 @api_view(["GET", "DELETE"])
 def get_delete_image(request, id):
     # Get image instance from ID
-    image = get_object_or_404(Image, id=id)
+    try:
+        image = Image.objects.get(id=int(pdf_id))
+    except:
+        return JsonResponse(
+            {"error": "Image not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     # Return detailed image by ID
     if request.method == "GET":
@@ -178,7 +181,12 @@ def get_delete_image(request, id):
 @api_view(["GET", "DELETE"])
 def get_delete_pdf(request, id):
     # Get pdf instance from ID
-    pdf = get_object_or_404(PDF, id=id)
+    try:
+        pdf = PDF.objects.get(id=int(pdf_id))
+    except:
+        return JsonResponse(
+            {"error": "PDF not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     # Return detailed pdf by ID
     if request.method == "GET":
@@ -194,16 +202,18 @@ def get_delete_pdf(request, id):
         )
 
 
-from PIL import ImageOps
-
-
 @api_view(["POST"])
 def rotate_image_view(request):
     image_id = request.data.get("image_id")
     rotation_angle = request.data.get("rotation_angle")
-
+    
     # Get image object or raise 404 ID sent does not exist
-    image = get_object_or_404(Image, id=image_id)
+    try:
+        image = Image.objects.get(id=int(pdf_id))
+    except:
+        return JsonResponse(
+            {"error": "Image not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     # Error handle if rotation angle is not a number
     try:
@@ -310,108 +320,3 @@ def convert_pdf_to_image(request):
         },
         status=status.HTTP_200_OK,
     )
-
-
-# @api_view(["POST"])
-# def convert_pdf_to_image(request):
-#     pdf_id = request.data.get("pdf_id")
-#     try:
-#         pdf = PDF.objects.get(id=pdf_id)
-#     except ObjectDoesNotExist:
-#         return JsonResponse(
-#             {"error": "PDF not found"}, status=status.HTTP_404_NOT_FOUND
-#         )
-
-#     images = convert_from_path(pdf.file.path)  # Convert PDF to images
-#     generated_image_paths = []
-
-#     for i, image in enumerate(images):
-#         image_dir = "images/"
-#         os.makedirs(
-#             image_dir, exist_ok=True
-#         )  # Create the 'images' directory if it doesn't exist
-#         pdf_name = os.path.basename(pdf.file.name)  # Extract the PDF file name
-#         image_path = os.path.join(image_dir, f"{pdf_name}_page{i+1}.jpg")
-
-#         image.save("media/" + image_path, "JPEG")
-#         Image.objects.create(file=image_path, uploaded_at=pdf.uploaded_at)
-#         generated_image_paths.append(
-#             "/media/" + image_path
-#         )  # Add the generated image path to the list
-
-#     pdf.file.delete()  # Delete the original PDF file
-#     pdf.delete()  # Delete the PDF object
-
-#     return JsonResponse(
-#         {
-#             "message": "PDF converted to image(s) successfully",
-#             "image_paths": generated_image_paths,
-#         },
-#         status=status.HTTP_200_OK,
-#     )
-
-
-# @api_view(["POST"])
-# def upload_file(request):
-#     if request.method == "POST":
-#         encoded_file = request.FILES["file"]
-#         file_extension = encoded_file.name.split(".")[-1].lower()
-#         serialized_data = None
-
-#         if is_pdf(file_extension):
-#             num_pages = get_num_pdf_pages(encoded_file)
-#             print(num_pages)
-#             serialized_data = PDFSerializerDetailed(
-#                 data={
-#                     "file": encoded_file,
-#                     "num_pages": num_pages,
-#                 }
-#             )
-#         elif is_image(file_extension):
-#             serialized_data = ImageSerializer(data={"file": encoded_file})
-#         else:
-#             return Response(
-#                 {"error": "Unsupported Media Type"},
-#                 status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-#             )
-
-#         if serialized_data.is_valid():
-#             document = serialized_data.save()
-
-#             if is_pdf(file_extension):
-#                 with open(document.file.path, "rb") as pdf_file:
-#                     pdf_reader = PyPDF2.PdfReader(pdf_file)
-#                     page = pdf_reader.pages[
-#                         0
-#                     ]  # Assuming you want dimensions of the first page
-#                     page_width = page.mediabox[2] - page.mediabox[0]
-#                     page_height = page.mediabox[3] - page.mediabox[1]
-
-#                 # Update the PDF model instance with calculated values
-#                 document.page_width = page_width
-#                 document.page_height = page_height
-#                 document.save()
-
-#             elif is_image(file_extension):
-#                 # Open the image using PIL
-#                 with PILImage.open(document.file.path) as image:
-#                     width, height = image.size
-#                     number_of_channels = len(image.getbands())
-
-#                 # Update the image model instance with calculated values
-#                 document.width = width
-#                 document.height = height
-#                 document.number_of_channels = number_of_channels
-#                 document.save()
-
-#             return Response(
-#                 {"message": "File uploaded successfully", "document_id": document.pk},
-#                 status=status.HTTP_200_OK,
-#             )
-#         else:
-#             return Response({"error": serialized_data.errors}, status=status.HTTP_400_BAD_REQUEST)
-#     else:
-#         return Response(
-#             {"error": "Invalid request method"},
-#             status=status.HTTP_405_METHOD_NOT_ALLOWED,
-#         )
